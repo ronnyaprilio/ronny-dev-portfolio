176688b8be97d8e3f9e578959110507486e930d2
Ronny <ronny@kazumaronz.com>
2025-12-19 03:42:11 +0700

    fix(api): add auth check to upload-image endpoint

--- src/app/api/upload-image/route.ts
+++ src/app/api/upload-image/route.ts
@@ -1,27 +1,41 @@
+import { verifyAuthentication } from "@/lib/admin/login/auth";
 import { getCloudinary } from "@/lib/cloudinary";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
-  const cloudinary = getCloudinary();
-  const formData = await req.formData();
-  const file = formData.get("file") as File;
+  try{
+    const session = await verifyAuthentication();
+      
+    if (!session.user || !session.session) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+    const cloudinary = getCloudinary();
+    const formData = await req.formData();
+    const file = formData.get("file") as File;
 
-  const buffer = Buffer.from(await file.arrayBuffer());
+    const buffer = Buffer.from(await file.arrayBuffer());
 
-  const result = await new Promise<any>((resolve, reject) => {
-    cloudinary.uploader
-      .upload_stream(
-        { folder: cloudinary.config().folder },
-        (error, result) => {
-          if (error) reject(error);
-          resolve(result);
-        }
-      )
-      .end(buffer);
-  });
+    const result = await new Promise<any>((resolve, reject) => {
+      cloudinary.uploader
+        .upload_stream(
+          { folder: cloudinary.config().folder },
+          (error, result) => {
+            if (error) reject(error);
+            resolve(result);
+          }
+        )
+        .end(buffer);
+    });
 
-  return NextResponse.json({
-    url: result.secure_url,
-    public_id: result.public_id,
-  });
+    return NextResponse.json({
+      url: result.secure_url,
+      public_id: result.public_id,
+    });
+  }catch(error){
+    console.error("Error uploading image:", error);
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
 }
